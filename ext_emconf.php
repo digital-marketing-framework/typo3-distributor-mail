3229810c7de68a5f33dce6853ca88f31e4082637
Stephan Ude <ude@mediatis.de>
2025-01-27 16:24:05 +0100

    2.3.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,11 +7,12 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Damian Ickler',
     'author_email' => 'info@mediatis.de',
     'author_company' => 'Mediatis AG',
-    'version' => '2.2.2',
+    'version' => '2.3.0',
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.0-12.4.99',
             'dmf_distributor_core' => '2.2.2-2.99.99',
+            'dmf_mail' => '1.0.0-1.99.99',
             'dmf_template_engine_twig' => '2.0.0-2.99.99',
         ],
         'conflicts' => [
