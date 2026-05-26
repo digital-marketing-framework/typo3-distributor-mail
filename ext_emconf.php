3f0b021814aba7f765b544a7d6a4e14dadb28e1b
s-ude <ude@mediatis.de>
2026-05-26 18:48:28 +0200

    [TASK] TYPO3 14 Compatibility (#7)

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,13 +7,13 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Damian Ickler',
     'author_email' => 'info@mediatis.de',
     'author_company' => 'Mediatis AG',
-    'version' => '3.0.2',
+    'version' => '4.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-13.4.99',
-            'dmf_distributor_core' => '3.0.0-3.99.99',
-            'dmf_mail' => '2.0.0-2.99.99',
-            'dmf_template_engine_twig' => '3.0.0-3.99.99',
+            'typo3' => '12.4.0-14.99.99',
+            'dmf_distributor_core' => '4.0.0-4.99.99',
+            'dmf_mail' => '3.0.0-3.99.99',
+            'dmf_template_engine_twig' => '4.0.0-4.99.99',
         ],
         'conflicts' => [
         ],
